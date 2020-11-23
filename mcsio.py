487f338fd869056bf3ede4cce0806a6385ff2e38
aiden00713 <jacky6849@gmail.com>
2020-11-23 14:03:11 +0800

    new file:   mcsio.py

---
+++ mcsio.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    SwitchStatus = GPIO.input(24)
+    if(SwitchStatus == 0):
+        print('Button pressed')
+    else:
+        print('Button released')
+
+    payload = {"datepoints":[{"dateChnId":"Hum","values":{"value":humidity}},{"dataChnId":"Temp","values":{"value":temperature}},{"dataChnId":"SwitchStatus","values":{value":SwitchStatus}}]}
+
+
